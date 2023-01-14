b303dcecbc9f5742fbc27d51091a9f1394578692
Edmundo Pichardo <ce.pichardo@gmail.com>
2023-01-14 05:15:53 -0400

    Introduccion a Yargs de NPM

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const { crearArchivo } = require('./helpers/multiplicar');
 
+const argv = require('yargs').argv;
+
 // impriman la tabla del 5
 console.clear();
 
 console.log(process.argv);
+console.log(argv);
+
+console.log('Base: yargs', argv.base);
 
 
-const base = 4;
-crearArchivo(base)
-   .then(nombreArchivo => console.log(nombreArchivo))
-   .catch( err => console.error(err));
+// const base = 4;
+// crearArchivo(base)
+//    .then(nombreArchivo => console.log(nombreArchivo))
+//    .catch( err => console.error(err));
