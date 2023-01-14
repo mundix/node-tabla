468b8586e2e8639ebe58efc4d4192ff62a1cf246
Edmundo Pichardo <ce.pichardo@gmail.com>
2023-01-14 04:50:04 -0400

    probadno process.argv

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const { crearArchivo } = require('./helpers/multiplicar');
 // impriman la tabla del 5
 console.clear();
 
+console.log(process.argv);
+
 
 const base = 4;
-crearArchivo(base)
-   .then(nombreArchivo => console.log(nombreArchivo))
-   .catch( err => console.error(err));
+// crearArchivo(base)
+//    .then(nombreArchivo => console.log(nombreArchivo))
+//    .catch( err => console.error(err));
